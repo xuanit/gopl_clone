95c67e09b90e79da1c423889653e504260fc14e1
Xuan Nguyen <xnguyenit@gmail.com>
2018-07-27 07:56:43 +0700

    update chat server

--- ch8/chat/chat.go
+++ ch8/chat/chat.go
@@ -9,13 +9,18 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 //!+broadcaster
-type client chan<- string // an outgoing message channel
+type client struct {
+	ch   chan<- string // an outgoing message channel
+	name string
+}
 
 var (
 	entering = make(chan client)
@@ -31,20 +36,28 @@ func broadcaster() {
 			// Broadcast incoming message to all
 			// clients' outgoing message channels.
 			for cli := range clients {
-				cli <- msg
+				cli.ch <- msg
 			}
 
 		case cli := <-entering:
 			clients[cli] = true
+			var buf bytes.Buffer
+			fmt.Fprintf(&buf, "There are")
+			for existingCli := range clients {
+				fmt.Fprintf(&buf, " %s,", existingCli.name)
+			}
+			cli.ch <- buf.String()
 
 		case cli := <-leaving:
 			delete(clients, cli)
-			close(cli)
+			close(cli.ch)
 		}
 	}
 }
 
 //!-broadcaster
+
+type bye bool
 
 //!+handleConn
 func handleConn(conn net.Conn) {
@@ -54,15 +67,38 @@ func handleConn(conn net.Conn) {
 	who := conn.RemoteAddr().String()
 	ch <- "You are " + who
 	messages <- who + " has arrived"
-	entering <- ch
+	entering <- client{name: who, ch: ch}
+	active := make(chan bye)
+	go func() {
+		input := bufio.NewScanner(conn)
+		for input.Scan() {
+			messages <- who + ": " + input.Text()
+			active <- bye(false)
+		}
+		active <- bye(true)
+	}()
 
-	input := bufio.NewScanner(conn)
-	for input.Scan() {
-		messages <- who + ": " + input.Text()
+	var IdleTime = 30 * time.Second
+	ticker := time.NewTicker(IdleTime)
+	var lastActive = time.Now()
+loop:
+	for {
+		select {
+		case bye := <-active:
+			if bye {
+				break loop
+			} else {
+				lastActive = time.Now()
+			}
+		case <-ticker.C:
+			if time.Since(lastActive) > IdleTime {
+				break loop
+			}
+		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
 
-	leaving <- ch
+	leaving <- client{ch: ch, name: who}
 	messages <- who + " has left"
 	conn.Close()
 }
