505ddd1df31b42550b8f01a401a230c3987c08a9
Xuan Nguyen <xnguyenit@gmail.com>
2018-07-27 07:59:13 +0700

    do exercise

--- ch8/crawl2/findlinks.go
+++ ch8/crawl2/findlinks.go
@@ -13,6 +13,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime/pprof"
 
 	"gopl.io/ch5/links"
 )
@@ -22,10 +23,10 @@ import (
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
 
-func crawl(url string) []string {
+func crawl(url string, cancel <-chan struct{}) []string {
 	fmt.Println(url)
 	tokens <- struct{}{} // acquire a token
-	list, err := links.Extract(url)
+	list, err := links.Extract(url, cancel)
 	<-tokens // release the token
 
 	if err != nil {
@@ -45,20 +46,28 @@ func main() {
 	n++
 	go func() { worklist <- os.Args[1:] }()
 
+	cancel := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		close(cancel)
+	}()
+
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
+		fmt.Printf("n is %d\n", n)
 		list := <-worklist
 		for _, link := range list {
 			if !seen[link] {
 				seen[link] = true
 				n++
 				go func(link string) {
-					worklist <- crawl(link)
+					worklist <- crawl(link, cancel)
 				}(link)
 			}
 		}
 	}
+	pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
 }
 
 //!-
